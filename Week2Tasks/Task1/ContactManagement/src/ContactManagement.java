3ba76e746c1a0794406b002b6fdb9f6f9e795810
Sarika <sarikav2003@gmail.com>
2024-08-06 17:14:23 +0530

    week2 task1

--- Week2Tasks/Task1/ContactManagement/src/ContactManagement.java
+++ Week2Tasks/Task1/ContactManagement/src/ContactManagement.java
@@ -1,9 +1,22 @@
 import java.util.Hashtable;
+import java.util.Map;
 
 public class ContactManagement {
     private Hashtable<Integer, Contact> contacts = new Hashtable<>();
     public void addContact(Contact contact) {
-        contact(contact.getId(), contact);
-        contact.
+        contacts.put(contact.getId(), contact);
+    }
+    public Contact removeContact(int contactId) {
+        if(contacts.containsKey(contactId)) {
+            return contacts.remove(contactId);
+        }
+        return null;
+    }
+    public void displayProducts() {
+        if(!contacts.isEmpty()) {
+            for(Map.Entry<Integer, Contact> contactEntry : contacts.entrySet()) {
+                System.out.println(contactEntry.getValue());
+            }
+        }
     }
 }
--- Week2Tasks/Task1/ContactManagement/src/ContactManagementTest.java
+++ Week2Tasks/Task1/ContactManagement/src/ContactManagementTest.java
@@ -1,3 +1,16 @@
 public class ContactManagementTest {
-
+    public static void main(String[] args) {
+        Contact contact1 = new Contact(1, "abc", "1234567890");
+        Contact contact2 = new Contact(2, "abc2", "1234567890");
+        Contact contact3 = new Contact(3, "abc3", "1234567890");
+        Contact contact4 = new Contact(4, "abc4", "1234567890");
+        ContactManagement contacts = new ContactManagement();
+        contacts.addContact(contact1);
+        contacts.addContact(contact2);
+        contacts.addContact(contact3);
+        contacts.addContact(contact4);
+        contacts.displayProducts();
+        contacts.removeContact(3);
+        contacts.displayProducts();
+    }
 }
