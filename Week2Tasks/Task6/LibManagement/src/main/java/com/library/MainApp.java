ced754cff304bda3eb37800e65dfafafba259f6f
Sarika <sarikav2003@gmail.com>
2024-08-11 13:51:29 +0530

    tsk6

---
+++ Week2Tasks/Task6/LibManagement/src/main/java/com/library/MainApp.java
@@ -0,0 +1,19 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        // Load the application context from the XML configuration file
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContent.xml");
+
+        // Retrieve the BookService bean from the application context
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Call the service method
+        bookService.performService();
+    }
+}
