04ccc86e8a8c0e1d458789dd669ab54de5465df3
Sarika <sarikav2003@gmail.com>
2024-08-12 16:59:28 +0530

    task2

---
+++ Week2Tasks/Task6/librarySB/librarySB/src/main/java/com/librarySB/librarySB/LibrarySbApplication.java
@@ -0,0 +1,13 @@
+package com.librarySB.librarySB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySbApplication.class, args);
+	}
+
+}
---
+++ Week2Tasks/Task6/librarySB/librarySB/src/test/java/com/librarySB/librarySB/LibrarySbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarySB.librarySB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
